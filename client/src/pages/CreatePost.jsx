b5c3adaaef7e8e4af4187cc1a765b9f5c53fa4f3
Geronimo Viscontti <visconttig@gmail.com>
2023-01-20 23:12:31 -0300

    Create random generator function & create post page.

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -1,11 +1,27 @@
+import { useState } from "react";
+import React from "react";
+import { useNavigate } from "react-router-dom";
+
+import { preview } from "../assets";
+import { getRandomPromt } from "../utils";
+import { FormField, Loader } from "../components";
 
 
 const CreatePost = () => {
+    const navigate = useNavigate();
+    const [form, setForm] = useState({
+        name: "",
+        prompt: "",
+        photo: ""
+    });
+    const [generatingImg, setGeneratingImg] = useState(false);
+    const [loading, setLoading] = useState(false);
+
 
     return (
-        <div>
-            Create Post Page
-        </div>
+        <section  >
+
+        </section>
     );
 }
 
