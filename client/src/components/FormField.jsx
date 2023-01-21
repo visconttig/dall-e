3c2fa93c662253c20544be846e6167c80bb816f6
Geronimo Viscontti <visconttig@gmail.com>
2023-01-21 11:37:31 -0300

    ...

--- client/src/components/FormField.jsx
+++ client/src/components/FormField.jsx
@@ -9,7 +9,28 @@ const FormField = ({labelName, type, name, placeholder, value, handleChange, isS
                 <label htmlFor={name} className="block text-sm font-medium text-gray-900" >
                     {labelName}
                 </label>
+                { isSurpriseMe && (
+                    <button 
+                        type="button"
+                        onClick={handleSurpriseMe}
+                        className="font-semibold text-xs bg-[#ececf1] px-1 
+                        px-2 rounded-[5px] text-black" >
+                        Surprise me!
+                    </button>
+                )}
             </div>
+            <input
+                type={type}
+                name={name}
+                id={name}
+                placeholder={placeholder}
+                value={value}
+                onChange={handleChange}
+                required
+                autoComplete="off"
+                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm 
+                rounded-lg focus:ring-[#4649ff] focus:border-[#4649ff] outline-none 
+                block w-full p-3" />
         </div>
     );
 }
--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -44,18 +44,39 @@ const CreatePost = () => {
                         name="name"
                         labelName="Your name"
                         type="text"
-                        placeholder="John Doe"
+                        placeholder="Elon Musk"
                         value={form.name}
                         handleChange={handleChange} />
                     <FormField
                         name="prompt"
                         labelName="Prompt"
                         type="text"
-                        placeholder="This is a placeholder text"
+                        placeholder="A Man falling in Love with his Computer, digital art"
                         value={form.prompt}
                         handleChange={handleChange}
                         isSurpriseMe
                         handleSurpriseMe />
+                </div>
+
+                <div className="relative bg-gray-50 border border-gray-300
+                     text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 w-64 p-4 h-64 flex justify-center items-center" >
+                        {form.photo ? (
+                            <img src={form.photo}
+                                alt={form.prompt}
+                                className="w-full h-full object-contain" />
+                        ) : (
+                            
+                            <img src={preview}
+                                alt="preview"
+                                className="w-9/12 h-9/12 object-contain opacity-40" />
+                        )}
+
+                        {generatingImg && (
+                            <div className="absolute inset-0 z-0 flex justify-center 
+                                items-center bg-[rgba(0,0,0,0.5)] rounded-lg">
+                                <Loader />
+                            </div>
+                        )}
                 </div>
 
             </form>
